4ea3bcd31d6a4d6db092d214bc45c66110f04112
Vivek Singh <viveksingh956038@gmail.com>
2025-08-12 19:18:30 +0530

    Add header component

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <div>  Vivek   
+   <div className="pt-16"> Vivek   
     <br />
     <Button variant={"destructive"}> Click me</Button>
    </div> 
--- components/convex-client-provider.jsx
+++ components/convex-client-provider.jsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
   return (
-    <ConvexProvider 
+    <ConvexProviderWithClerk 
         client={convex}
-        // useAuth={}
+        useAuth={useAuth}
     >
         {children}
-    </ConvexProvider>
+    </ConvexProviderWithClerk>
   );
 }
